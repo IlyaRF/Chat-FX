9bf3210d2bd8d941f9647c4d5a772d55d6ad1700
IlyaRF <ilyagaitanov@gmail.com>
2022-06-20 00:17:14 +0300

    Chat FX

---
+++ src/main/java/com/example/javafxchat/client/ChatClient.java
@@ -0,0 +1,79 @@
+package com.example.javafxchat.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ChatClient {
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+
+    private final ChatController controller;
+
+    public ChatClient(ChatController controller) {
+        this.controller = controller;
+    }
+
+    public void openConnection() throws IOException {
+        socket = new Socket("localhost", 8189);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        new Thread(() -> {
+            try {
+                readMessages();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                closeConnection();
+            }
+        }).start();
+    }
+
+    private void closeConnection() {
+        if (in != null) {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                closeConnection();
+            }
+        }
+        if (out != null) {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void readMessages() throws IOException {
+        while (true) {
+            final String message = in.readUTF();
+            if ("/end".equals(message)) {
+                break;
+            }
+            controller.addMessage(message);
+        }
+    }
+
+    public void sendMessage(String message) {
+        try {
+            out.writeUTF(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/javafxchat/client/ChatController.java
+++ src/main/java/com/example/javafxchat/client/ChatController.java
@@ -1,27 +1,60 @@
 package com.example.javafxchat.client;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class ChatController {
 
     @FXML
-    public TextField messageField;
+    private TextField messageField;
 
     @FXML
-    public TextArea messageArea;
+    private TextArea messageArea;
+
+    private final ChatClient client;
+
+    public ChatController() {
+        this.client = new ChatClient(this);
+        while (true) {
+            try {
+                client.openConnection();
+                break;
+            } catch (IOException e) {
+                showNotification();
+            }
+        }
+    }
+
+    private void showNotification() {
+        final Alert alert = new Alert(Alert.AlertType.ERROR,
+                "Не могу подключиться к серрверу",
+                new ButtonType("Попробовать снова", ButtonBar.ButtonData.OK_DONE),
+                new ButtonType("Выйти", ButtonBar.ButtonData.CANCEL_CLOSE)
+        );
+        alert.setTitle("Ошибка подключения");
+        Optional<ButtonType> answer = alert.showAndWait();
+        Boolean isExit = answer.map(select -> select.getButtonData().isCancelButton()).orElse(false);
+    }
 
     public void clickSendButton() {
 
-            final String message = messageField.getText();
-            if (message.isBlank()) {
-                return;
-            }
+        final String message = messageField.getText();
+        if (message.isBlank()) {
+            return;
+        }
 
-            messageArea.appendText(message + "\n");
-            messageField.clear();
-            messageField.requestFocus();
+        client.sendMessage(message);
+        messageField.clear();
+        messageField.requestFocus();
 
+    }
+
+    public void addMessage(String message) {
+        if (message.isBlank()) {
+            return;
         }
     }
+}
